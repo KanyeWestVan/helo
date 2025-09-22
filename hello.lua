d847f4d96b08ef552d3537bc4b2767bf491f6eed
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2025-09-22 17:57:51 -0400

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -1,1 +1,11 @@
-print("hello")
+game.Players.LocalPlayer.Backpack:WaitForChild('BatKnightBatSword')
+Chr = game.Players.LocalPlayer.Character
+game.Players.LocalPlayer.Backpack.BatKnightBatSword.Parent = Chr
+Chr.BatKnightBatSword.Server.BatTransform.DisableBackpack:Destroy()
+Chr.BatKnightBatSword.Remote:FireServer(Enum.KeyCode.Q)
+wait()
+Chr.BatKnightBatSword.Parent = game.Players.LocalPlayer.Backpack
+wait(0.5)
+Chr.Humanoid.JumpPower = 50
+Chr.Humanoid.AutoRotate = true
+wait()
